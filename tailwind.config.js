00be148a79286adda66a93b3726f6768a7701ac7
gknook <gknook@gmail.com>
2020-11-21 21:59:26 +0100

    tailwind colors setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
+// tailwind.config.js
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: { content: ["./public/**/*.html", "./src/**/*.vue"] },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {},
+    colors: {
+      gray: colors.warmGray,
+      blue: colors.lightBlue,
+      red: colors.rose,
+      yellow: colors.amber,
+      green: colors.lime
+    }
   },
   variants: {
     extend: {}
